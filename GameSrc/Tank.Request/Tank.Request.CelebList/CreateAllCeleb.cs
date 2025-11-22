c82fa7f92e6016870bb9c0096b4abf59ad743f56
vanloc19 <tovanloc19@gmail.com>
2025-11-23 03:57:06 +0700

    fix(ranking): Remove IP check from CreateAllCeleb handler
    - Remove IP check to allow public access from game client and debug tools
    - Enables ranking data generation for all clients

--- GameSrc/Tank.Request/Tank.Request.CelebList/CreateAllCeleb.cs
+++ GameSrc/Tank.Request/Tank.Request.CelebList/CreateAllCeleb.cs
@@ -12,42 +12,34 @@ namespace Tank.Request.CelebList
 
 		public void ProcessRequest(HttpContext context)
 		{
-			var userAdress = context.Request.UserHostAddress;
-			//var userAdress =  context.Request.Headers.Get("X-Real-IP").ToString();
-			if (csFunction.ValidAdminIP(userAdress))
-			{
-				StringBuilder stringBuilder = new StringBuilder();
-				stringBuilder.Append(CelebByGpList.Build());
-				stringBuilder.Append(CelebByDayGPList.Build());
-				stringBuilder.Append(CelebByWeekGPList.Build());
-				stringBuilder.Append(CelebByOfferList.Build());
-				stringBuilder.Append(CelebByDayOfferList.Build());
-				stringBuilder.Append(CelebByWeekOfferList.Build());
-				stringBuilder.Append(CelebByDayFightPowerList.Build());
-				stringBuilder.Append(CelebByConsortiaRiches.Build());
-				stringBuilder.Append(CelebByConsortiaDayRiches.Build());
-				stringBuilder.Append(CelebByConsortiaWeekRiches.Build());
-				stringBuilder.Append(CelebByConsortiaHonor.Build());
-				stringBuilder.Append(CelebByConsortiaDayHonor.Build());
-				stringBuilder.Append(CelebByConsortiaWeekHonor.Build());
-				stringBuilder.Append(CelebByConsortiaLevel.Build());
-				stringBuilder.Append(CelebByDayBestEquip.Build());
-				stringBuilder.Append(celebbyconsortiafightpower.Build());
-				stringBuilder.Append(CelebByWeekLeagueScore.Build());
-				stringBuilder.Append(CelebByAchievementPointDayList.Build());
-				stringBuilder.Append(CelebByAchievementPointList.Build());
-				stringBuilder.Append(CelebByAchievementPointWeekList.Build());
-				stringBuilder.Append(CelebByGiftGpList.Build());
-				stringBuilder.Append(CelebByDayGiftGp.Build());//Edit
-				stringBuilder.Append(CelebByWeekGiftGP.Build());
-				stringBuilder.Append(elitematchplayerlist.Build());
-				context.Response.ContentType = "text/plain";
-				context.Response.Write(stringBuilder.ToString());
-			}
-			else
-			{
-				context.Response.Write("IP is not valid!");
-			}
+			// Removed IP check to allow public access from game client and debug tools
+			StringBuilder stringBuilder = new StringBuilder();
+			stringBuilder.Append(CelebByGpList.Build());
+			stringBuilder.Append(CelebByDayGPList.Build());
+			stringBuilder.Append(CelebByWeekGPList.Build());
+			stringBuilder.Append(CelebByOfferList.Build());
+			stringBuilder.Append(CelebByDayOfferList.Build());
+			stringBuilder.Append(CelebByWeekOfferList.Build());
+			stringBuilder.Append(CelebByDayFightPowerList.Build());
+			stringBuilder.Append(CelebByConsortiaRiches.Build());
+			stringBuilder.Append(CelebByConsortiaDayRiches.Build());
+			stringBuilder.Append(CelebByConsortiaWeekRiches.Build());
+			stringBuilder.Append(CelebByConsortiaHonor.Build());
+			stringBuilder.Append(CelebByConsortiaDayHonor.Build());
+			stringBuilder.Append(CelebByConsortiaWeekHonor.Build());
+			stringBuilder.Append(CelebByConsortiaLevel.Build());
+			stringBuilder.Append(CelebByDayBestEquip.Build());
+			stringBuilder.Append(celebbyconsortiafightpower.Build());
+			stringBuilder.Append(CelebByWeekLeagueScore.Build());
+			stringBuilder.Append(CelebByAchievementPointDayList.Build());
+			stringBuilder.Append(CelebByAchievementPointList.Build());
+			stringBuilder.Append(CelebByAchievementPointWeekList.Build());
+			stringBuilder.Append(CelebByGiftGpList.Build());
+			stringBuilder.Append(CelebByDayGiftGp.Build());//Edit
+			stringBuilder.Append(CelebByWeekGiftGP.Build());
+			stringBuilder.Append(elitematchplayerlist.Build());
+			context.Response.ContentType = "text/plain";
+			context.Response.Write(stringBuilder.ToString());
 		}
 	}
 }
