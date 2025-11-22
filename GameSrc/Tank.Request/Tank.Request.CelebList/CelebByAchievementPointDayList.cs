be5796ffcee4b6d1aec3962c5a34a10e3618f395
vanloc19 <tovanloc19@gmail.com>
2025-11-23 03:56:45 +0700

    fix(ranking): Add cache headers to Achievement ranking handlers
    - Add no-cache headers to prevent client-side caching
    - Use compressed XML response for better performance
    - Remove IP check to allow public access from game client
    - Day/Week handlers use total ranking data for consistency

--- GameSrc/Tank.Request/Tank.Request.CelebList/CelebByAchievementPointDayList.cs
+++ GameSrc/Tank.Request/Tank.Request.CelebList/CelebByAchievementPointDayList.cs
@@ -1,7 +1,10 @@
 using System.Reflection;
 using System.Web;
 using System.Web.Services;
+using System.Xml.Linq;
 using log4net;
+using Road.Flash;
+using Bussiness;
 
 namespace Tank.Request.CelebList
 {
@@ -15,21 +18,39 @@ namespace Tank.Request.CelebList
 
 		public void ProcessRequest(HttpContext context)
 		{
-			context.Response.Write(Build(context));
+			// Prevent caching to ensure fresh data
+			context.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+			context.Response.Cache.SetNoStore();
+			context.Response.ContentType = "application/octet-stream";
+
+			// Return same data as CelebByAchievementPointList (Tổng) - always fresh from database
+			XElement result = CelebByAchievementPointList.BuildXml();
+			if (result != null)
+			{
+				context.Response.BinaryWrite(StaticFunction.Compress(result.ToString(check: false)));
+			}
+			else
+			{
+				context.Response.Write("CelebByAchievementPointDayList Fail!");
+			}
 		}
 
 		public static string Build(HttpContext context)
 		{
-			if (!csFunction.ValidAdminIP(context.Request.UserHostAddress))
-			{
-				return "CelebByAchievementPointDayList Fail!";
-			}
 			return Build();
 		}
 
 		public static string Build()
 		{
-			return csFunction.BuildCelebUsers("CelebByAchievementPointDayList", 11, "CelebByAchievementPointDayList_Out");
+			// Use same data as CelebByAchievementPointList (Tổng) but create CelebByAchievementPointDayList.xml file
+			XElement result = CelebByAchievementPointList.BuildXml();
+			return csFunction.CreateCompressXml(result, "CelebByAchievementPointDayList", isCompress: true);
+		}
+
+		public static XElement BuildXml()
+		{
+			// Use same data as CelebByAchievementPointList (Tổng)
+			return CelebByAchievementPointList.BuildXml();
 		}
 	}
 }
